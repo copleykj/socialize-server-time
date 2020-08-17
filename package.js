868ae2e5b97205c6cb9fe63d2f0dff1fa427dd5e
Kelly Copley <copleykj@gmail.com>
2020-08-17 09:15:07 -0400

    fix publishing node_modules

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
     name: 'socialize:server-time',
     summary: 'Package to compensate for client/server time difference',
-    version: '1.0.1',
+    version: '1.0.2',
     git: 'https://github.com/copleykj/socialize-server-time.git',
 });
 
