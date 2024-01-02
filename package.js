9668919005b6ea2e0baebd04c0be29c54918fa93
Jan Dvorak <storytellercz@proton.me>
2024-01-02 21:38:14 +0100

    Add support for Meteor 3.0-beta.0

--- package.js
+++ package.js
@@ -2,14 +2,15 @@
 Package.describe({
     name: 'socialize:server-time',
     summary: 'Package to compensate for client/server time difference',
-    version: '1.0.3',
+    version: '1.0.4',
     git: 'https://github.com/copleykj/socialize-server-time.git',
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom(['1.10.2', '2.3']);
+    api.versionsFrom(['1.10.2', '2.3', '3.0-beta.0']);
 
     api.use('ecmascript');
+    api.use('ddp');
 
     api.mainModule('server/server-time.js', 'server');
     api.mainModule('client/entry-meteor.js', 'client');
